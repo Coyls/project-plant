0c50e053f60acd4b1f533100e5483c93b2f469a6
Coyls <loic.grecofr@gmail.com>
2022-04-15 00:21:59 +0200

    refactor : add speak.py + make rotary work with volume

--- plant/rotary.py
+++ plant/rotary.py
@@ -1,5 +1,8 @@
 from RPi import GPIO
 from time import sleep
+import alsaaudio
+
+m = alsaaudio.Mixer()
 
 clk = 17
 dt = 18
@@ -8,21 +11,24 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-counter = 0
+counter = 50
 clkLastState = GPIO.input(clk)
 
 try:
 
-        while True:
-                clkState = GPIO.input(clk)
-                dtState = GPIO.input(dt)
-                if clkState != clkLastState:
-                        if dtState != clkState:
-                                counter += 1
-                        else:
-                                counter -= 1
-                        print(counter) 
-                clkLastState = clkState
-                sleep(0.01)
+    while True:
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+        if clkState != clkLastState:
+            if dtState != clkState:
+                if counter < 100:
+                    counter += 1
+            else:
+                if counter > 0:
+                    counter -= 1
+            m.setvolume(counter)
+            print(m.getvolume())
+        clkLastState = clkState
+        sleep(0.01)
 finally:
-        GPIO.cleanup()
+    GPIO.cleanup()
---
+++ plant/speak.py
@@ -0,0 +1,6 @@
+import subprocess
+
+text = "Nearly ten years had passed since the Dursleys had woken up to find their nephew on the front step, but Privet Drive had hardly changed at all. The sun rose on the same tidy front gardens and lit up the brass number four on the Dursleys' front door; it crept into their living room, which was almost exactly the same as it had been on the night when Mr. Dursley had seen that fateful news report about the owls. Only the photographs on the mantelpiece really showed how much time had passed."
+
+subprocess.run(["espeak", "-g", "3", text])
+
